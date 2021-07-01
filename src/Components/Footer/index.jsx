72bec1f253cf3c5b66d85ca48e5b5b11d01321f8
Rafa Garrido <rafagarridolara@gmail.com>
2021-07-01 01:42:08 -0400

    Add Footer styles

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,15 +1,4 @@
-import styled from "styled-components";
-
-export const FooterDiv = styled.div`
-  background: black;
-  color: white;
-  display: flex;
-  flex-direction: column-reverse;
-  align-items: center;
-  padding-bottom: 10px;
-  height: 100px;
-  font-size: 18px;
-`;
+import { FooterDiv } from "./styles";
 
 const Footer = () => {
   return (
---
+++ src/Components/Footer/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterDiv = styled.div`
+  background: black;
+  color: white;
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+  padding-bottom: 10px;
+  height: 100px;
+  font-size: 18px;
+`;
