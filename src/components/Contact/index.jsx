ae19804aff27507d3c6d136dceb37b6e217dd96a
Rafa Garrido <rafagarridolara@gmail.com>
2021-07-19 12:48:50 -0400

    Change contact title

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,7 +4,7 @@ import { ContactDiv } from "./styled";
 const Contact = () => {
   return (
     <ContactDiv id="contact">
-      <h2>Want to do something cool?</h2>
+      <h2>Want to make something cool?</h2>
       <p>Let's work together 😀</p>
       <Button variant="secundary" url="mailto:garridolara.rafa@gmail.com">
         GET IN TOUCH
